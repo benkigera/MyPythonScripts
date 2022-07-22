191afbee36350522055d7a14787ebcd6ac2873db
benkigera <benkigera@gmail.com>
2022-07-22 12:41:39 +0300

    Add geocode script

---
+++ Geocoding API/geocoding.py
@@ -0,0 +1,17 @@
+import json
+
+import httplib2
+
+
+def getGeocodeLocation(inputString):
+    google_api_key = "AIzaSyC8IwBXxSyZZrBmR3X_YkPRaFyAcmtgvhg"
+    locationString = inputString.replace(" ", "+")
+    url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s' % (
+        locationString, google_api_key))
+    h = httplib2.Http()
+    response, content = h.request(url, 'GET')
+    result = json.loads(content)
+    # print response
+    latitude = result['results'][0]['geometry']['location']['lat']
+    longitude = result['results'][0]['geometry']['location']['lng']
+    return (latitude, longitude)
